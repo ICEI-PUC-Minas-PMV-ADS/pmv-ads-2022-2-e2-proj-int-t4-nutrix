de94d02a856a36d9628977576e3e3d8349888cec
Philipe Rocha <philiperochad.js@gmail.com>
2022-12-08 20:22:38 -0300

    Ajustes visuais

--- src/Nutrix-DIETAS-E-ACOMPANHAMENTO-CSHARP/Controllers/HomeController.cs
+++ src/Nutrix-DIETAS-E-ACOMPANHAMENTO-CSHARP/Controllers/HomeController.cs
@@ -1,28 +1,124 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Nutrix_DIETAS_E_ACOMPANHAMENTO_CSHARP.Models;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace Nutrix_DIETAS_E_ACOMPANHAMENTO_CSHARP.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public async Task<IActionResult> WaterCalc()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                return View();
-            }
-            else
+            if (!User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Login", "Usuarios");
             }
+
+            string userIdValue = "";
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+
+            try
+            {
+                if (claimsIdentity != null)
+                {
+                    var userIdClaim = claimsIdentity.Claims
+                        .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                    if (userIdClaim != null)
+                    {
+                        userIdValue = userIdClaim.Value;
+                    }
+                }
+
+
+
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Usuário não pôde ser selecionado, tente efetuar o login novamente.";
+
+                return View();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.Email == userIdValue);
+
+            var dadosPessoais = await _context.DadoPessoais
+                .OrderBy(x => x.Id)
+                .LastOrDefaultAsync(m => m.UsuarioId == usuario.Id);
+
+            if (dadosPessoais == null)
+            {
+                ViewBag.Message = "Dados pessoais não encontrados";
+
+                return RedirectToAction("DadosForm", "DadoPessoais");
+            }
+
+            string AguaPorPeso = (dadosPessoais.Peso * 35).ToString() + "ML por dia";
+
+            return View("Views/Home/Water.cshtml", AguaPorPeso);
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Usuarios");
+            }
+
+            string userIdValue = "";
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+
+            try
+            {
+                if (claimsIdentity != null)
+                {
+                    var userIdClaim = claimsIdentity.Claims
+                        .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+                    if (userIdClaim != null)
+                    {
+                        userIdValue = userIdClaim.Value;
+                    }
+                }
+
+
+
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Usuário não pôde ser selecionado, tente efetuar o login novamente.";
+
+                return View();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.Email == userIdValue);
+
+
+
+            var dietas = _context.Dietas
+                .Where(m => m.UsuarioId == usuario.Id)
+                .OrderByDescending(c => c.Id)
+                .Take(3);
+
+            return View(dietas);
         }
 
         public IActionResult Privacy()
